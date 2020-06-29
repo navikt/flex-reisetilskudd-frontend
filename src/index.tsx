dded8c20d99137cfbfe990d6855aa66ae359421c
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-06-29 15:07:04 +0200

    rearranges files and refactors

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+
+import App from './App';
+
+
+var mountNode = document.getElementById("root");
+ReactDOM.render(<App name="Team-flex" />, mountNode);
+
