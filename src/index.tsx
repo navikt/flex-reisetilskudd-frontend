fdeefcc272276faad8ed486b209c8482754eb14d
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-06-30 15:43:17 +0200

    Initializes project with webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import App from './App';
+import { Hello } from "./components/hello";
 
-
-var mountNode = document.getElementById("root");
-ReactDOM.render(<App name="Team-flex" />, mountNode);
-
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
+);
