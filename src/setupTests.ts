338ab4632964a891b69b913d069168d7b0b48b37
Sigurd Øines <sigurd.oines2@nav.no>
2020-07-02 12:18:59 +0200

    Satt opp eslint med Airbnb TS style guide. Fikset lint-feil

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,4 +6,5 @@ import '@testing-library/jest-dom/extend-expect';
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
