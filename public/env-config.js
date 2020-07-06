ca4f76ff75f41e37d268c00484baec61bb281c60
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-07-06 11:23:15 +0200

    legger til dekoratoren

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,5 @@
 window._env_ = {
   REISEPENGER_URL: "http://localhost:1337",
   API_URL: "http://localhost:6969/reisetilskudd/",
+  REACT_APP_DECORATOR_URL: "http://localhost:8100/dekoratoren",
 };
