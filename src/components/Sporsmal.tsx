a690b9d16cf294978dde8ddb987d2e5f8b81c88f
benedihm <benemyr17@gmail.com>
2020-07-07 10:17:14 +0200

    WIP: spørsmålskomponent

--- src/components/Sporsmal.tsx
+++ src/components/Sporsmal.tsx
@@ -1,6 +1,29 @@
-import React, { ReactElement } from 'react';
+import 'nav-frontend-skjema-style';
+import React, { ReactElement, useState } from 'react';
+import { RadioPanelGruppe } from 'nav-frontend-skjema';
 
-function RadioPG() : ReactElement {
+const RadioPG= () => {
+  const [active, setActive] = useState('juice1id')
+
+  return(
+  <RadioPanelGruppe
+    name="samplename"
+    legend="Hvilken drikke er best?"
+    radios={[
+        { label: 'Eplejuice', value: 'juice1', id: 'juice1id' },
+        { label: 'Appelsinjuice', value: 'juice2', id: 'juice2id' },
+        { label: 'Melk', value: 'melk', id: 'melkid' },
+        { label: 'Ananasjuice', value: 'juice3', id: 'juice4id' }
+    ]}
+    checked={'juice2id'}
+    onChange= {() => setActive('juice2id')}
+/>)
+
+}
+
+
+
+/*function RadioPG() : ReactElement {
   return (
     <div className="radioContainer" key="melk">
       <input
@@ -8,11 +31,13 @@ function RadioPG() : ReactElement {
         id="melk"
         name="melk"
         value="melk"
-        checked
-        className="skjemaelement__input radioknapp"
+        checked= {true}
+        className="skjemaelement__label"
       />
+
+      <label className= "skjemaelement__label" htmlFor= "Ja-svar">Ja</label>
     </div>
   );
-}
+}*/
 
 export default RadioPG;
