b3a6fb44705dc1702c85a2814a482cc31b94c871
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-07-09 15:50:11 +0200

    legger til normaltext til lenkene i app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import { Normaltekst } from 'nav-frontend-typografi';
 import Soknaden from './pages/soknaden/soknaden';
 import Utbetaling from './pages/utbetaling/utbetaling';
 import DagensTransportmiddel from './pages/dagens-transportmiddel/dagens-transportmiddel';
@@ -11,40 +12,42 @@ function App() : ReactElement {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Hjem</Link>
-            </li>
-            <li>
-              <Link to="/utbetaling">Utbetaling</Link>
-            </li>
-            <li>
-              <Link to="/dagens-transportmiddel">Dagens transportmiddel</Link>
-            </li>
-            <li>
-              <Link to="/reisetilskudd-periode">Reisetilskudd periode</Link>
-            </li>
-          </ul>
-        </nav>
+        <Normaltekst>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Hjem</Link>
+              </li>
+              <li>
+                <Link to="/utbetaling">Utbetaling</Link>
+              </li>
+              <li>
+                <Link to="/dagens-transportmiddel">Dagens transportmiddel</Link>
+              </li>
+              <li>
+                <Link to="/reisetilskudd-periode">Reisetilskudd periode</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {
+          {
         }
 
-        <Switch>
-          <Route path="/utbetaling">
-            <Utbetaling />
-          </Route>
-          <Route path="/dagens-transportmiddel">
-            <DagensTransportmiddel />
-          </Route>
-          <Route path="/reisetilskudd-periode">
-            <ReiseTilskuddPeriode />
-          </Route>
-          <Route path="/">
-            <Soknaden />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route path="/utbetaling">
+              <Utbetaling />
+            </Route>
+            <Route path="/dagens-transportmiddel">
+              <DagensTransportmiddel />
+            </Route>
+            <Route path="/reisetilskudd-periode">
+              <ReiseTilskuddPeriode />
+            </Route>
+            <Route path="/">
+              <Soknaden />
+            </Route>
+          </Switch>
+        </Normaltekst>
       </div>
     </Router>
   );
