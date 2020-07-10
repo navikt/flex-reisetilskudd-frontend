ea3654f05158528726d9c2120807b55c39e338dd
Sigurd Øines <sigurd.oines2@nav.no>
2020-07-10 10:50:31 +0200

    La til Flatpickr som datovelger

--- src/components/dato/ReisetilskuddDatovelger.tsx
+++ src/components/dato/ReisetilskuddDatovelger.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
-import { Datovelger } from 'nav-datovelger';
+import Flatpickr from 'react-flatpickr';
+import { Norwegian } from 'flatpickr/dist/l10n/no';
+import './flatpickr.less';
 
 const ReisetilskuddDatovelger: React.FC = () => {
-  const [dato, setDato] = useState('');
+  const [dato, setDato] = useState(new Date());
 
-  const validerDato = (d : string | null | undefined) => {
-    if (typeof d === 'string') {
-      setDato(d);
-    }
+  const validerDato = (d : Date[]) => {
+    setDato(d[0]);
   };
 
   return (
-    <Datovelger valgtDato={dato} onChange={(d) => validerDato(d)} />
+    <Flatpickr
+      id="datoTest"
+      name="datoTest"
+      className="skjemaelement__input input--m"
+      value={dato}
+      placeholder="dd.mm.åååå"
+      options={{
+        mode: 'single',
+        enableTime: false,
+        dateFormat: 'Y-m-d',
+        altInput: true,
+        altFormat: 'd.m.Y',
+        locale: Norwegian,
+        allowInput: true,
+        disableMobile: true,
+      }}
+      onChange={(date) => validerDato(date)}
+    />
   );
 };
 
