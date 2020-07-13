0122e6636cc66c2f1e4c38c0e49b131acdde30cf
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-07-13 11:05:15 +0200

    lintfix

--- src/pages/kvittering-side/kvittering-side.tsx
+++ src/pages/kvittering-side/kvittering-side.tsx
@@ -1,64 +1,14 @@
 import React, { ReactElement } from 'react';
-import AlertStripe from 'nav-frontend-alertstriper';
 import './kvittering-side.less';
-import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import ListeTekstbox from './liste-tekstbox';
+import VeienVidereBox from './veien-viderebox';
 
 function KvitteringSide():ReactElement {
   return (
     <div className="page-wrapper">
-      <p>morra di er mann</p>
-      <div className="liste-wrapper">
-        <div className="tekstwrapper">
-          <Normaltekst>
-            <Undertittel>Nå har du gjort første del</Undertittel>
-            <p>Du har sendt beskjed om sykefraværet til arbeidsgiveren din</p>
-            <Undertittel>Du har nå fullført andre del</Undertittel>
-            <p>
-              <p>
-                Kravet ditt om reisetilskudd er nå under behandling.
-                Du vil få varsel når dette er klart
-              </p>
-              <b>Lurer du på hva saksbehandler gjør?</b>
-              <p><a href="www.nav.no">Les mer om reglene for reisetilskudd .</a></p>
-            </p>
-          </Normaltekst>
-        </div>
-      </div>
-      <div className="veien-videre-wrapper">
-        <AlertStripe className="gronn-checkbox" type="suksess">
-          <Undertittel>Sykemeldingen er sendt til NAV</Undertittel>
-          <p>
-            Dato sendt:
-            {}
-            , kl:
-            {}
-          </p>
-        </AlertStripe>
-        <AlertStripe className="bla-info" type="info">
-          <Undertittel>Hva skjer videre?</Undertittel>
-          <p>
-            <b>Du trenger ikke å søke om sykepenger</b>
-            <p>
-              NAV dekker ikke sykepenger de første 16 dagene.
-              Dette sykefraværet er kortere, derfor trenger du ikke søke.
-            </p>
-            <p>
-              Les gjerne om
-              <a href="www.nav.no">sykepenger til selvstendig næringsdrivende og frilansere</a>
-            </p>
-          </p>
-          <p>
-            <b>Har du flere jobber?</b>
-            <p>
-              Du må levere én sykemelding per jobb.
-              Kontakt den som har sykemeldt deg hvis du trenger flere sykemeldinger.
-            </p>
-          </p>
-          <p>
-            <b>Skal du ut og reise?</b>
-            <p><a href="www.nav.no">Les om hva du må gjøre for å beholde sykepengene.</a></p>
-          </p>
-        </AlertStripe>
+      <div className="content-wrapper">
+        <ListeTekstbox />
+        <VeienVidereBox />
       </div>
     </div>
   );
---
+++ src/pages/kvittering-side/liste-tekstbox.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement } from 'react';
+import './kvittering-side.less';
+import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+
+function ListeTekstbox():ReactElement {
+  return (
+    <div className="liste-wrapper">
+      <div className="tekstwrapper">
+        <Normaltekst>
+          <div className="inline-container">
+            <div className="numberCircle">1</div>
+            {' '}
+            <span />
+            <div>
+              <Undertittel>Nå har du gjort første del</Undertittel>
+              <p>Du har sendt beskjed om sykefraværet til arbeidsgiveren din</p>
+            </div>
+          </div>
+
+          <div className="inline-container">
+            <div className="numberCircle">2</div>
+            <span />
+            <div>
+              <Undertittel>Du har nå fullført andre del</Undertittel>
+              <p>
+                <p>
+                  Kravet ditt om reisetilskudd er nå under behandling.
+                  Du vil få varsel når dette er klart
+                </p>
+                <b>Lurer du på hva saksbehandler gjør?</b>
+                <p><a href="www.nav.no">Les mer om reglene for reisetilskudd .</a></p>
+              </p>
+            </div>
+          </div>
+        </Normaltekst>
+      </div>
+    </div>
+  );
+}
+
+export default ListeTekstbox;
