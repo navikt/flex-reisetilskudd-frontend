33466d042c0783ac9c641d8f308605130e060496
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-07-13 15:09:18 +0200

    resolves merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -3,54 +3,12 @@ import {
   BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 import { Normaltekst } from 'nav-frontend-typografi';
-import Soknaden from './pages/soknaden/soknaden'
-import Utbetaling from './pages/utbetaling/utbetaling';
-import DagensTransportmiddel from './pages/dagens-transportmiddel/dagens-transportmiddel';
-import ReiseTilskuddPeriode from './pages/reisetilskudd-periode/reisetilskudd-periode';
+import Soknaden from './pages/soknaden/soknaden';
 import KvitteringSide from './pages/kvittering-side/kvittering-side';
 
 function App() : ReactElement {
   return (
     <Router>
-      <div>
-        <Normaltekst>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Hjem</Link>
-              </li>
-              <li>
-                <Link to="/utbetaling">Utbetaling</Link>
-              </li>
-              <li>
-                <Link to="/dagens-transportmiddel">Dagens transportmiddel</Link>
-              </li>
-              <li>
-                <Link to="/reisetilskudd-periode">Reisetilskudd periode</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {
-        }
-
-          <Switch>
-            <Route path="/utbetaling">
-              <Utbetaling />
-            </Route>
-            <Route path="/dagens-transportmiddel">
-              <DagensTransportmiddel />
-            </Route>
-            <Route path="/reisetilskudd-periode">
-              <ReiseTilskuddPeriode />
-            </Route>
-            <Route path="/">
-              <Soknaden />
-            </Route>
-          </Switch>
-        </Normaltekst>
-        <KvitteringSide />
-      </div>
       <Normaltekst>
         <Switch>
           <Route path="/soknaden/:id">
