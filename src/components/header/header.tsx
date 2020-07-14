64ffae6315c8c65ff9a8773b1810fe85f1d04b30
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-07-14 13:40:54 +0200

    lint fixes

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+import './header.less';
+
+function Header():ReactElement {
+  return (
+    <div className="header">
+      <h1>morra di</h1>
+    </div>
+  );
+}
+
+export default Header;
