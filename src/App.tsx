303cacda40ca0c1fc40c0d26530156dc2382c64f
ulrikah <ulrikhalmoy@gmail.com>
2020-07-14 09:37:59 +0200

    Fjerner Normaltekst-wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react';
 import {
   BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
-import { Normaltekst } from 'nav-frontend-typografi';
 import Soknaden from './pages/soknaden/soknaden';
 import KvitteringSide from './pages/kvittering-side/kvittering-side';
 import ReiseTilskuddPeriode from './pages/reisetilskudd-periode/reisetilskudd-periode';
@@ -10,19 +9,17 @@ import ReiseTilskuddPeriode from './pages/reisetilskudd-periode/reisetilskudd-pe
 function App() : ReactElement {
   return (
     <Router>
-      <Normaltekst>
-        <Switch>
-          <Route path="/soknaden/:id">
-            <Soknaden />
-          </Route>
-          <Route path="/perioder">
-            <ReiseTilskuddPeriode />
-          </Route>
-          <Route path="/kvittering">
-            <KvitteringSide />
-          </Route>
-        </Switch>
-      </Normaltekst>
+      <Switch>
+        <Route path="/soknaden/:id">
+          <Soknaden />
+        </Route>
+        <Route path="/perioder">
+          <ReiseTilskuddPeriode />
+        </Route>
+        <Route path="/kvittering">
+          <KvitteringSide />
+        </Route>
+      </Switch>
     </Router>
   );
 }
