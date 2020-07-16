456ce70c27c8245a403b1957be1eecf99dbde23c
ulrikah <ulrikhalmoy@gmail.com>
2020-07-16 09:01:27 +0200

    Wrapper tag-styling i wrapper-klasse

--- src/pages/bekreftelsesside/BekreftelsesSide.tsx
+++ src/pages/bekreftelsesside/BekreftelsesSide.tsx
@@ -5,7 +5,7 @@ import VeienVidereBox from './VeienVidere';
 
 function BekreftelsesSide():ReactElement {
   return (
-    <div className="page-wrapper">
+    <div className="bekreftelsesside-page-wrapper">
       <div className="content-wrapper">
         <ListeTekstbox />
         <VeienVidereBox />
