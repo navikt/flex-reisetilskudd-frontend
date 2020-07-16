57c0d32583c21a39baf5e6f22eed997356c790b1
benedihm <benemyr17@gmail.com>
2020-07-16 17:09:43 +0200

    WIP: Ordnet på plasseringen av filnavn, beløp, og dato

--- src/components/filopplaster/FilMedInfo.tsx
+++ src/components/filopplaster/FilMedInfo.tsx
@@ -7,36 +7,36 @@ import formaterFilstørrelse from './utils';
 import { IVedlegg } from '../../models/vedlegg';
 
 interface Props {
-    fil: IVedlegg;
-    slettVedlegg?: (vedlegg: IVedlegg) => void;
-  }
+  fil: IVedlegg;
+  slettVedlegg?: (vedlegg: IVedlegg) => void;
+}
 
-const FilMedInfo: React.FC<Props> = ({fil, slettVedlegg }) => (
-    <div className="fil">
-        <div>
-        <img
-            className="vedleggsikon"
-            src={vedlegg}
-            alt="Vedleggsikon"
-        />
-        <Normaltekst className="filnavn">{fil.navn}</Normaltekst>
-        <Normaltekst className="filstørrelse">
-            (
-            {formaterFilstørrelse(fil.størrelse)}
-            )
-        </Normaltekst>
-        </div>
-        <Normaltekst>{fil.beløp}</Normaltekst>
-        <Normaltekst>{fil.dato?.toDateString()}</Normaltekst>
-        {slettVedlegg
-        ? (
-            <Fareknapp mini onClick={() => { slettVedlegg(fil); }}>
-            <SlettIkon />
-            <span>SLETT</span>
-            </Fareknapp>
+const FilMedInfo: React.FC<Props> = ({ fil, slettVedlegg }) => (
+  <div className="fil">
+    <div className="kvittering">
+      <img
+        className="vedleggsikon"
+        src={vedlegg}
+        alt="Vedleggsikon"
+      />
+      <Normaltekst className="filnavn">{fil.navn}</Normaltekst>
+      <Normaltekst className="filstørrelse">
+        (
+        {formaterFilstørrelse(fil.størrelse)}
         )
-        : <></>}
-    </div>
-)
+      </Normaltekst>
+      </div>
+        <Normaltekst className="beløp">{fil.beløp}</Normaltekst>
+        <Normaltekst className="dato">{fil.dato?.toDateString()}</Normaltekst>
+    {slettVedlegg
+      ? (
+        <Fareknapp mini onClick={() => { slettVedlegg(fil); }}>
+          <SlettIkon />
+          <span>SLETT</span>
+        </Fareknapp>
+      )
+      : <></>}
+  </div>
+);
 
 export default FilMedInfo;
--- src/components/filopplaster/OpplastedeFiler.tsx
+++ src/components/filopplaster/OpplastedeFiler.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import FilMedInfo from './FilMedInfo'
+import FilMedInfo from './FilMedInfo';
 import { IVedlegg } from '../../models/vedlegg';
+import { Undertittel } from 'nav-frontend-typografi';
+import Vis from '../../components/Vis';
+import './Filopplaster'
 
 interface Props {
   filliste: IVedlegg[];
@@ -10,6 +13,14 @@ interface Props {
 
 const OpplastedeFiler: React.FC<Props> = ({ filliste, slettVedlegg, className }) => (
   <div className={className}>
+    <Vis hvis={filliste.length > 0}>
+      <div className="kvitteringsinfo-tittel">
+        <Undertittel>Kvittering</Undertittel>
+        <Undertittel>Beløp</Undertittel>
+        <Undertittel>Dato</Undertittel>
+      </div>
+    </Vis>
+
     {filliste.map((fil: IVedlegg, index: number) => (
       <div key={fil.navn}>
         <FilMedInfo fil={fil} slettVedlegg={slettVedlegg} />
