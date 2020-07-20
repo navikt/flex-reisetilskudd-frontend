8c09a80da45484d65ce79ca3a6e317882cdddcf5
ulrikah <ulrikhalmoy@gmail.com>
2020-07-20 11:16:32 +0200

    Legger til oppsummeringsside for søknaden

--- src/pages/oppsummering/Oppsummering.tsx
+++ src/pages/oppsummering/Oppsummering.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react';
+import PeriodeOppsummering from '../../components/oppsummering/PeriodeOppsummering';
 
 function Oppsummering():ReactElement {
   return (
-    <>
-    </>
+    <div>
+      <PeriodeOppsummering />
+    </div>
   );
 }
 
