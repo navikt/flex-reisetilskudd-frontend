339de4254178c3599fcc5fbadc74e81cbe385b1f
Sigurd Øines <sigurd.oines2@nav.no>
2020-07-21 14:31:11 +0200

    Bytter ut arbeidsgiverinfo i utbetalingsspørsmål

--- src/pages/utbetaling/Utbetaling.tsx
+++ src/pages/utbetaling/Utbetaling.tsx
@@ -1,11 +1,41 @@
 import React, { ReactElement } from 'react';
 import RadioSpørsmålUtbetaling from '../../components/sporsmal/radioSpørsmål/RadioSpørsmålUtbetaling';
 import { utbetalingSpørsmål } from '../../components/sporsmal/spørsmålTekster';
+import { ArbeidsgiverInterface } from '../../models/arbeidsgiver';
+import { arbeidsgiverNavnPlaceHolder, arbeidsgiverOrgNrPlaceHolder } from './constants';
+import { Svaralternativ } from '../../types/types';
 
-const Utbetaling = (): ReactElement => (
-  <>
-    {RadioSpørsmålUtbetaling(utbetalingSpørsmål)}
-  </>
-);
+const Utbetaling = (): ReactElement => {
+  const getArbeidsgiver = () : ArbeidsgiverInterface => ({
+    navn: 'Arbeids- og velferdsetaten',
+    orgNr: '392392482849',
+  });
+
+  const leggInnArbeidsGiverIString = (tekstStreng : string) => tekstStreng.replace(
+    arbeidsgiverNavnPlaceHolder, getArbeidsgiver().navn,
+  ).replace(
+    arbeidsgiverOrgNrPlaceHolder, getArbeidsgiver().orgNr,
+  );
+
+  const byttUtSpørsmålsTekster = (svaralternativer : Svaralternativ[]) : Svaralternativ[] => {
+    const nyeAlternativer = [...svaralternativer];
+    nyeAlternativer.forEach((svaralternativ : Svaralternativ) => {
+      // TODO: Finne ut hvordan man kan gjøre dette uten å suppresse no-param-reassign
+      // return {...svaralternativ, label: leggInnArbeidsGiverIString(svaralternativ.label)};
+      // eslint-disable-next-line no-param-reassign
+      svaralternativ.label = leggInnArbeidsGiverIString(svaralternativ.label);
+    });
+    return nyeAlternativer;
+  };
+
+  return (
+    <RadioSpørsmålUtbetaling
+      tittel={utbetalingSpørsmål.tittel}
+      name={utbetalingSpørsmål.name}
+      spørsmålstekst={leggInnArbeidsGiverIString(utbetalingSpørsmål.spørsmålstekst)}
+      svaralternativer={byttUtSpørsmålsTekster(utbetalingSpørsmål.svaralternativer)}
+    />
+  );
+};
 
 export default Utbetaling;
