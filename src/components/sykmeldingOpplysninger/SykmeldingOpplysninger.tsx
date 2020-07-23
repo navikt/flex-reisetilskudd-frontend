4ff2324b4c3488573f8184e0d3fe3eaecf15b933
Sigurd Øines <sigurd.oines2@nav.no>
2020-07-23 10:43:12 +0200

    La til enkel feilmelding, refaktorerte litt

--- src/components/sykmeldingOpplysninger/SykmeldingOpplysninger.tsx
+++ src/components/sykmeldingOpplysninger/SykmeldingOpplysninger.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import './sykmeldingOpplysninger.less';
-import { Element } from 'nav-frontend-typografi';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { useAppStore } from '../../data/stores/app-store';
 import Vis from '../Vis';
 import { SykmeldingOpplysningEnum } from '../../models/sykmelding';
@@ -13,22 +13,29 @@ const SykmeldingOpplysninger = () : ReactElement => {
     <div>
       <Vis hvis={vårSykmelding !== undefined}>
         <Element>Periode</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.FRA_DATO] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.FRA_DATO]}
         {' '}
         -
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.TIL_DATO] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.TIL_DATO]}
         <Element>Diagnose</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.DIAGNOSE] : ''}
-        <Element>Bidiagnose</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.BI_DIAGNOSE] : ''}
-        <Element>Beskriv fraværet</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.BESKRIV_FRAVÆR] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.DIAGNOSE]}
+        <Vis hvis={vårSykmelding?.[SykmeldingOpplysningEnum.BI_DIAGNOSE]}>
+          <Element>Bidiagnose</Element>
+          {vårSykmelding?.[SykmeldingOpplysningEnum.BI_DIAGNOSE]}
+        </Vis>
+        <Element>Beskrivelse av fraværet</Element>
+        {vårSykmelding?.[SykmeldingOpplysningEnum.BESKRIV_FRAVÆR]}
         <Element>Beskriv eventelle hesyn som må tas på arbeidsplassen</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.BESKRIV_HENSYN] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.BESKRIV_HENSYN]}
         <Element>Arbeidsgiver som legen har skrevet inn</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.ARBEIDSGIVER] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.ARBEIDSGIVER]}
         <Element>Lege/sykmelder</Element>
-        {vårSykmelding ? vårSykmelding[SykmeldingOpplysningEnum.SYKMELDER] : ''}
+        {vårSykmelding?.[SykmeldingOpplysningEnum.SYKMELDER]}
+      </Vis>
+      <Vis hvis={vårSykmelding === undefined}>
+        <Normaltekst>
+          Vi kunne dessverre ikke hente opplysninger fra sykmeldingen din, prøv igjen senere
+        </Normaltekst>
       </Vis>
     </div>
   );
