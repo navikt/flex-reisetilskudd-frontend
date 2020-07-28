4abc9fca22da3c78f573e99c2a1d08f46a722947
Karoline Bernklev <karoline.bernklev@nav.no>
2020-07-28 16:53:05 +0200

    Legger til hjelpetekst for kvitteringsopplastning, lager egen hjelpetekst-fil

--- src/pages/kvitteringsopplasting/Kvitteringsopplasting.tsx
+++ src/pages/kvitteringsopplasting/Kvitteringsopplasting.tsx
@@ -3,16 +3,23 @@ import {
   Innholdstittel, Ingress,
 } from 'nav-frontend-typografi';
 import './kvitteringsopplasting.less';
+import Hjelpetekst from 'nav-frontend-hjelpetekst';
 import FilopplasterModal from '../../components/filopplaster/filopplasterModal/FilopplasterModal';
 import OpplastedeFiler from '../../components/filopplaster/OpplastedeFiler';
 // import DragAndDrop from '../../components/filopplaster/DragAndDrop';
 import DragAndDrop from '../../components/filopplaster/dragAndDrop/DragAndDrop';
 import TotalBelop from '../../components/kvittering/totaltBelop/TotaltBelop';
+import { hjelpetekstKvitteringopplasting } from '../../constants/hjelpetekster';
 
-const Kvitteringsopplasting: React.FC = () : ReactElement => (
+const Kvitteringsopplasting: React.FC = (): ReactElement => (
   <div className="perioder-wrapper">
     <Innholdstittel className="perioder-overskrift">Opplasting av kvitteringer</Innholdstittel>
-    <Ingress className="perioder-overskrift"> Legg inn dine kvitteringer </Ingress>
+    <div className="kvitteringinfo-wrapper">
+      <Ingress className="perioder-ingress"> Legg inn dine kvitteringer </Ingress>
+      <Hjelpetekst className="kvitteringsopplasting-hjelpetekst">
+        {hjelpetekstKvitteringopplasting.hjelpetekst}
+      </Hjelpetekst>
+    </div>
     <div className="filopplaster-wrapper periode-element">
       <div className="filopplaster">
         <FilopplasterModal />
