de695758f25eb2a919e58fe14cad7f4677b43fde
Karoline Bernklev <karoline.bernklev@nav.no>
2020-07-30 16:47:50 +0200

    Rydder opp i navn i InputSpørsmål

--- src/components/sporsmal/inputSporsmal/InputSporsmal.tsx
+++ src/components/sporsmal/inputSporsmal/InputSporsmal.tsx
@@ -4,17 +4,18 @@ import { Input } from 'nav-frontend-skjema';
 import Hjelpetekst from 'nav-frontend-hjelpetekst';
 import { InputProps } from '../../../types/types';
 import './input-sporsmal.less';
+import { antallKilometerSpørsmål, månedligeUtgifterSpørsmål } from '../spørsmålTekster';
 import Vis from '../../Vis';
 import { hjelpetekstEgenBil, hjelpetekstKollektivtransport } from '../../../constants/hjelpetekster';
 
 const InputSporsmal = ({
   tittel, inputMode, bredde, value, onChange, id, feil,
 }: InputProps): ReactElement => (
-  <div className="månedlige-utgifter-spørsmål">
+  <div className="input-tittel">
     <Element>
       {tittel}
     </Element>
-    <div className="månedlige-utgifter-input">
+    <div className="input-felt">
       <Input
         inputMode={inputMode}
         pattern="[0-9]*"
@@ -29,14 +30,14 @@ const InputSporsmal = ({
         feil={feil}
       />
       <Vis
-        hvis={id === 'dagens-transportmiddel-kilometer-input'}
+        hvis={id === antallKilometerSpørsmål.id}
       >
         <Hjelpetekst className="transportmiddel-kilometer-hjelpetekst">
           {hjelpetekstEgenBil.hjelpetekst}
         </Hjelpetekst>
       </Vis>
       <Vis
-        hvis={id === 'dagens-transportmiddel-månedlige-utgifter-input'}
+        hvis={id === månedligeUtgifterSpørsmål.id}
       >
         <Hjelpetekst className="månedlige-utgifter-hjelpetekst">
           {hjelpetekstKollektivtransport.hjelpetekst}
