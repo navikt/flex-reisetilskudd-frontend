2410e9dfa50a91c33bf0f09b67c9aee95a0d2f93
Dennis Jianbin Liang <dennisjianbin@gmail.com>
2020-08-03 13:13:25 +0200

    rask endring av App.test, lint fix

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders "Søknad om reisetilskudd"', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const overskriftsElement = getByText(/Søknad om reisetilskudd/i);
+  expect(overskriftsElement).toBeInTheDocument();
 });
