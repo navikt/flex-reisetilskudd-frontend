c3c3bbac758990d1da93409cd1bb4b716a59bb7a
Kjell Arne Brødreskift <kjarbr@gmail.com>
2020-11-27 12:14:36 +0100

    Ny oppsummeringsside

--- src/components/diverse/banner/banner.tsx
+++ src/components/diverse/banner/banner.tsx
@@ -1,15 +1,21 @@
 import './banner.less'
 
 import { Sidetittel } from 'nav-frontend-typografi'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface BannerProps {
     tittel: string;
 }
 
 const Banner = ({ tittel }: BannerProps) => {
+    const bannerRef = useRef<HTMLElement>(null)
+
+    useEffect(() => {
+        bannerRef.current!.scrollIntoView({ behavior: 'smooth' })
+    }, [])
+
     return (
-        <header className="sidebanner">
+        <header ref={bannerRef} className="sidebanner">
             <Sidetittel tag="h1" className="sidebanner__tittel">
                 {tittel}
             </Sidetittel>
